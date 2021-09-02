b4de3e9d88bdd16fc0ab7bfcf6e02d7d84910e43
Omar Labana <omarlabana@outlook.com>
2021-09-02 23:58:51 +0200

    Prepare main components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            Hi
+        </div>
+    )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
+// import { Provider } from 'react-redux';
+import App from './components/App';
+// import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
