7b924e945f027b2bfa295de5be5dd0526b26f73e
Omar Labana <omarlabana@outlook.com>
2021-09-03 01:35:19 +0200

    Edit header comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+
 
 const Header = () => {
     return (
-        <div>
-            hey
-        </div>
+        <header className="flex items-center justify-between mx-3 py-3">
+            <span className="font-bold text-2xl text-blue-900">冬 Fuyu</span>
+            <span className="flex gap-2 text-white">
+                <Link to="/login" className="bg-black p-2 rounded">Login</Link>
+                <Link to="/register" className="bg-black p-2 rounded">Register</Link>
+            </span>
+        </header>
     )
 }
 
