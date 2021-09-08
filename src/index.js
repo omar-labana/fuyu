be9dee9c60de63413cdd994b8787894323086925
Omar Labana <omarlabana@outlook.com>
2021-09-08 14:27:41 +0200

    Add redux provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
-// import store from './redux/store';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
