9273bc55ae0954ab6eaaf94e10a6a54fdba528e0
Omar Labana <omarlabana@outlook.com>
2021-09-08 14:28:18 +0200

    Add routing and session check

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,33 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Home from '../views/Home';
-// import Meal from '../views/Meal';
+import Login from '../views/auth/Login';
+import Register from '../views/auth/Register';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getUser } from '../redux/slices/user';
 
-const App = () => (
-  <>
-    <Header />
-    <BrowserRouter>
-      <Switch>
-        {/* <Route path="/meal/:id" component={Meal} /> */}
-        <Route path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
-    <Footer />
-  </>
-);
+const App = () => {
+  const status = "NOT_LOGGED_IN"
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser(status));
+  }, [dispatch, status]);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          {/* <Route path="/meal/:id" component={Meal} /> */}
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
+};
 
 export default App;
