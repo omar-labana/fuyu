955b8ed216eefe295ece91a4186837431642201d
Omar Labana <omarlabana@outlook.com>
2021-09-08 14:28:53 +0200

    Add auth store

---
+++ src/redux/slices/user.js
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { checkLoginStatus } from '../../api-service';
+
+
+export const getUser = createAsyncThunk(
+    'user/getUser',
+    async (status) => {
+        const user = await checkLoginStatus(status)
+        return user
+    },
+);
+
+const initialState = {
+    user: {},
+    loggedInStatus: "NOT_LOGGED_IN",
+}
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    extraReducers: {
+        [getUser.fulfilled]: (state, { payload }) => {
+            state.loggedInStatus = payload.loggedInStatus
+            state.user = payload.user
+        },
+    },
+    reducers: {
+        updateUser: (state, action) => {
+            state.loggedInStatus = "LOGGED_IN"
+            state.user = action.payload
+        },
+        removeUser: (state) => {
+            state.loggedInStatus = "NOT_LOGGED_IN"
+            state.user = {}
+        },
+    },
+});
+
+export const { updateUser, removeUser } = userSlice.actions;
+export default userSlice.reducer;
