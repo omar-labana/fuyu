67c2d493c75db648ec842e83b6ef48bac753c4c2
Omar Labana <omarlabana@outlook.com>
2021-09-09 11:41:20 +0200

    Edit reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/user'
+import jacketsReducer from './slices/jackets'
 
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    jackets: jacketsReducer,
   },
 });
 
