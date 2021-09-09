ce5de44c6f36ac5377ba257cd0d44f9455747349
Omar Labana <omarlabana@outlook.com>
2021-09-09 21:51:28 +0200

    Replace footer comp

---
+++ src/components/Wish.js
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+
+import { addToWishlist } from "../api-service"
+
+const Wish = (props) => {
+    const { id } = props
+    const history = useHistory();
+    const status = useSelector((state) => state.user.loggedInStatus)
+    const user = useSelector((state) => state.user.user)
+    const handleClick = () => {
+        if (status === "NOT_LOGGED_IN") {
+            history.push("/login");
+        } else {
+            addToWishlist(user, id)
+        }
+    }
+    return (
+        <button type="button" onClick={handleClick} className=" w-full inset-x-0 bottom-0 py-3 bg-yellow-600 text-center  text-white text-xl font-semibold">
+            Add to your Wishlist
+        </button>
+    )
+}
+
+export default Wish
