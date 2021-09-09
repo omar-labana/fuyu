e94b2221b5962d04a88221cdcf1b39e71a357586
Omar Labana <omarlabana@outlook.com>
2021-09-09 11:40:04 +0200

    Fix footer at bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            Hi
-        </div>
+        <footer className="absolute inset-x-0 bottom-0 py-3 mx-2">
+            Footer
+        </footer>
     )
 }
 
