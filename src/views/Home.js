7c818ab70f09feb302d342a374d23117f64c02e6
Omar Labana <omarlabana@outlook.com>
2021-09-09 11:41:28 +0200

    Edit reducers

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { getJacketsArr } from '../redux/slices/jackets';
+import Jacket from '../components/Jacket';
 
 const Home = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getJacketsArr());
+    }, [dispatch]);
+    const jackets = useSelector((state) => state.jackets.jackets);
+    const [id, setId] = useState(0);
+
+    const target = jackets[id]
     return (
-        <div>
-            Home
+        <div className="flex items-center mx-2 py-3">
+            <i className="fas fa-arrow-left text-white bg-yellow-500 rounded-full p-3" onClick={() => setId(id - 1 === -1 ? 0 : id - 1)}></i>
+            <div>
+                {target ? <Jacket jacket={target} /> : ''}
+            </div>
+            <i className="fas fa-arrow-right text-white bg-yellow-500 rounded-full p-3" onClick={() => setId(id + 1 === jackets.length ? id : id + 1)}></i>
         </div>
     )
 }
