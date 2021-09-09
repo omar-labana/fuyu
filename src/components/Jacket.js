ba3d69733bae7779eba3af317f3689fe7febde47
Omar Labana <omarlabana@outlook.com>
2021-09-09 21:51:50 +0200

    Edit details nav

--- src/components/Jacket.js
+++ src/components/Jacket.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Jacket = (props) => {
     const { jacket } = props
-    const { name, price, image_url } = jacket
+    const { name, price, image_url, id } = jacket
     return (
         <div className="bg-white rounded-lg m-5 p-2">
             <img src={image_url} alt={name} />
@@ -11,8 +12,11 @@ const Jacket = (props) => {
                 <div className="font-bold">{price}</div>
             </div>
             <div className="text-sm flex justify-between my-2">
-                <button className="bg-yellow-400 px-2 py-1 rounded text-white"><i className="fas fa-star"></i> Whishlist </button>
-                <button className="bg-yellow-400 px-2 py-1 rounded text-white"> <i className="fas fa-info-circle"></i> Details</button>
+                <Link to={{
+                    pathname: `/jacket/${id}`,
+                    state: { id: id },
+                }}
+                    className="bg-yellow-400 px-2 py-1 rounded text-white w-full text-center"> <i className="fas fa-info-circle "></i> Details</Link >
             </div>
         </div>
     )
